2cfbd4ac2cf6926eb86061c40ab8d99dd5691af3
Guido Giuntoli <gagiuntoli@gmail.com>
2024-01-08 20:38:55 +0100

    Adding asserts

--- map_test.c
+++ map_test.c
@@ -20,6 +20,18 @@ void insert(Map *map, const char *key_c, const char *value_c) {
 	map_insert(map, key, value);
 }
 
+char *get(Map *map, const char *key_c) {
+	Key *key = malloc(sizeof(Key) + strlen(key_c) + 1);
+	key->size = strlen(key_c) + 1;
+	strcpy(key->data, key_c);
+
+	Value *value = map_get(map, key);
+	if (value != NULL) {
+		return (char *)value->data;
+	}
+	return NULL;
+}
+
 int main() {
 	Map *map = map_create();
 
@@ -28,8 +40,10 @@ int main() {
 	insert(map, "p3", "Saul");
 	insert(map, "A name", "A surname");
 
-	//Value *res = map_get(map, key);
-	//printf("key = %s val = %s\n", key->data, res->data);
+	assert(strcmp(get(map, "person 1"), "guido") == 0);
+	assert(strcmp(get(map, "person 2"), "peter") == 0);
+	assert(strcmp(get(map, "p3"), "Saul") == 0);
+	assert(strcmp(get(map, "A name"), "A surname") == 0);
 
 	map_print(map, print_key, print_value);
 
